88f5367baa83d3aead8a1572f2c799240701f64c
Mayank985 <mayankjni536@gmail.com>
2022-08-03 21:57:59 +0530

    updated

--- lib/apis/peer_to_peer_Questions.dart
+++ lib/apis/peer_to_peer_Questions.dart
@@ -1,23 +1,25 @@
 import 'dart:convert';
+
 import 'package:demo_app_flutter/constants.dart';
 import 'package:demo_app_flutter/data_fields.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
-
 // UserMark ID
-
 
 class PairToPairQuestionApi {
   static RxString status = "Loading".obs;
   static void fetchData(id, receiver) async {
     if (DataFields.isInternetOn.value) {
-      var url = Uri.parse(Constants.baseApiUrl + "written/answer-view-peer-to-peer/$id/$receiver/");
-      var response = await http.get(url,
-          headers: {"Authorization": "Token " + DataFields.token});
+      var url = Uri.parse(
+          "${Constants.baseApiUrl}written/answer-view-peer-to-peer/$id/$receiver/");
+      var response = await http
+          .get(url, headers: {"Authorization": "Token ${DataFields.token}"});
+      print("response == ${response.body}");
       if (response.statusCode == 200) {
         DataFields.teacherQuestion =
             teacherQuestionFromJson(utf8.decode(response.bodyBytes));
+        print("loaded");
         status.value = "Loaded";
       } else {
         status.value = "Error";
@@ -34,7 +36,6 @@ TeacherQuestion teacherQuestionFromJson(String str) =>
 String teacherQuestionToJson(TeacherQuestion data) =>
     json.encode(data.toJson());
 
-
 class TeacherQuestion {
   TeacherQuestion({
     this.response,
@@ -48,8 +49,8 @@ class TeacherQuestion {
       );
 
   Map<String, dynamic> toJson() => {
-    "Response": response?.toJson(),
-  };
+        "Response": response?.toJson(),
+      };
 }
 
 class Response {
@@ -64,17 +65,17 @@ class Response {
   List<Question>? questions;
 
   factory Response.fromJson(Map<String, dynamic> json) => Response(
-    examReviewId: json["exam_review_id"],
-    isTeacher: json["is_teacher"],
-    questions: List<Question>.from(
-        json["questions"].map((x) => Question.fromJson(x))),
-  );
+        examReviewId: json["exam_review_id"],
+        isTeacher: json["is_teacher"],
+        questions: List<Question>.from(
+            json["questions"].map((x) => Question.fromJson(x))),
+      );
 
   Map<String, dynamic> toJson() => {
-    "exam_review_id": examReviewId,
-    "is_teacher": isTeacher,
-    "questions": List<dynamic>.from(questions!.map((x) => x.toJson())),
-  };
+        "exam_review_id": examReviewId,
+        "is_teacher": isTeacher,
+        "questions": List<dynamic>.from(questions!.map((x) => x.toJson())),
+      };
 }
 
 class Question {
@@ -95,20 +96,20 @@ class Question {
   bool? isMarked;
 
   factory Question.fromJson(Map<String, dynamic> json) => Question(
-    id: json["id"],
-    providedMarks: json["provided_marks"].toDouble(),
-    image_count: json["image_count"],
-    question: json["question"],
-    realMark: json["real_mark"],
-    isMarked: json["is_marked"],
-  );
+        id: json["id"],
+        providedMarks: json["provided_marks"].toDouble(),
+        image_count: json["image_count"],
+        question: json["question"],
+        realMark: json["real_mark"],
+        isMarked: json["is_marked"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "provided_marks": providedMarks,
-    "image_count": image_count,
-    "question": question,
-    "real_mark": realMark,
-    "is_marked": isMarked,
-  };
+        "id": id,
+        "provided_marks": providedMarks,
+        "image_count": image_count,
+        "question": question,
+        "real_mark": realMark,
+        "is_marked": isMarked,
+      };
 }
